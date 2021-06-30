d51ea0241a601ed86090e292d695436229a22086
clara-kim <kim.clar@husky.neu.edu>
2021-06-30 19:11:07 -0400

    added utility for validating email address

--- ClarkCodingChallenge/Utilities/ValidityUtilities.cs
+++ ClarkCodingChallenge/Utilities/ValidityUtilities.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace ClarkCodingChallenge.Utilities
 {
-    public class RegexUtilities
+    public class ValidityUtilities
     {
+        // Method that checks the validity of an email address
+        // Param: string (email address to be validated)
+        // Returns: bool (true if valid / false if invalid)
+        public static bool IsValidEmail(string email)
+        {
+            email = email.Trim();
+
+            if (String.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+            else
+            {
+                return new System.ComponentModel.DataAnnotations.EmailAddressAttribute().IsValid(email);
+            }
+        }
     }
 }
