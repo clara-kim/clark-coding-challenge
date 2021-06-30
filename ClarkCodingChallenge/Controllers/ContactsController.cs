4155e298f9f0ff35e1aeaf47c0c8126ab4b1718c
clara-kim <kim.clar@husky.neu.edu>
2021-06-30 19:10:38 -0400

    added logic for validating, but alert boxes are missing

--- ClarkCodingChallenge/Controllers/ContactsController.cs
+++ ClarkCodingChallenge/Controllers/ContactsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ClarkCodingChallenge.Models;
+using ClarkCodingChallenge.Utilities;
 
 namespace ClarkCodingChallenge.Controllers
 {
@@ -14,6 +15,29 @@ namespace ClarkCodingChallenge.Controllers
         public IActionResult AddContacts()
         {
             return View();
+        }
+
+        public IActionResult AddContactConfirmation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContacts(string firstName, string lastName, string email)
+        {
+            // TODO: implement if you have time to look into displaying alert boxes
+            if (string.IsNullOrEmpty(firstName))
+            {
+                // show alert box "First name is required"
+            } else if (string.IsNullOrEmpty(firstName))
+            {
+                // show alert box "First name is required"
+            } else if (!ValidityUtilities.IsValidEmail(email))
+            {
+                // show alert box "Please provide a valid email address."
+            }
+            
+            return View("AddContactConfirmation");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
